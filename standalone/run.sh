f5d8cfefa6cf2511710690a2fb6301d61909882d
Andres Rios Tascon <ariostas@gmail.com>
2024-06-03 13:22:35 -0400

    Fixes

--- standalone/run.sh
+++ standalone/run.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
+# Set the "master" CMSSW branch
+MASTER_BRANCH=LSTCore_devel
+
 # Print all commands and exit on error
 set -e -v
 
@@ -7,7 +10,7 @@ set -e -v
 git checkout -b pr_branch
 git fetch --unshallow || echo "" # It might be worth switching actions/checkout to use depth 0 later on
 git config user.email "gha@example.com" && git config user.name "GHA" # For some reason this is needed even though nothing is being committed
-git merge --no-commit --no-ff origin/master || (echo "***\nError: There are merge conflicts that need to be resolved.\n***" && false)
+git merge --no-commit --no-ff origin/${MASTER_BRANCH} || (echo "***\nError: There are merge conflicts that need to be resolved.\n***" && false)
 
 # Download data files
 cd RecoTracker/LSTCore
@@ -28,7 +31,7 @@ python3 efficiency/python/lst_plot_performance.py LSTNumDen_after.root -t "valid
 # Checkout the master branch so we can compare what has changed
 git stash
 PRSHA=$(git rev-parse HEAD)
-git checkout origin/master
+git checkout origin/${MASTER_BRANCH}
 
 # Build and run master. Create comparison plots
 echo "Running setup script..."
